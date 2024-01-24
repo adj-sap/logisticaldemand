3a61982b619a0c88002261dca10ca5678b8b4d19
adj-sap <adarsh.kumar.jaiswal@sap.com>
2024-01-24 15:02:49 +0530

    gitignore fix

---
+++ srv/external.cds
@@ -0,0 +1,2 @@
+using { API_BUSINESS_PARTNER as external } from './external/API_BUSINESS_PARTNER';
+
---
+++ srv/src/main/java/customer/logisticaldemand/handlers/DemandServiceHandler.java
@@ -0,0 +1,31 @@
+package customer.logisticaldemand.handlers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+import com.sap.cds.services.handler.EventHandler;
+import com.sap.cds.services.handler.annotations.ServiceName;
+import com.sap.cds.services.persistence.PersistenceService;
+
+import cds.gen.api_business_partner.ApiBusinessPartner;
+import cds.gen.api_business_partner.ApiBusinessPartner_;
+import cds.gen.demandsservice.*;
+
+@Component
+@ServiceName(DemandsService_.CDS_NAME)
+public class DemandServiceHandler implements EventHandler {
+    
+    private final static Logger logger = LoggerFactory.getLogger(DemandServiceHandler.class);
+
+	// We are mashing up the AdminService with two other services...
+	private final PersistenceService db;
+	private final ApiBusinessPartner bupa;
+
+	DemandServiceHandler(PersistenceService db, @Qualifier(ApiBusinessPartner_.CDS_NAME) ApiBusinessPartner bupa) {
+		this.db = db;
+		this.bupa = bupa;
+	}
+
+}
